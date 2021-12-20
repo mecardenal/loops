ca2a8c278d0729dcf40dbe5c609e6b684717454f
mecardenal <44734512+mecardenal@users.noreply.github.com>
2021-12-20 23:02:46 +0100

    Update Script2.js

--- Script2.js
+++ Script2.js
@@ -1,13 +1,13 @@
 // JavaScript source code
 
-$( document ).ready(function() {
+//$( document ).ready(function() {
 
     for(let i = 0; i<= 10; i++){
        new PopUp('Lailololeilo lala',  { my: "right+" + i*10 + " top-" + i*10 + "%"}).showPopUp();
 
     }
     
-});
+//});
 
 class PopUp {
   constructor(message, position) {
