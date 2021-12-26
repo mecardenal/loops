c614646e803eefff2958388527b291e2afcf2163
mecardenal <44734512+mecardenal@users.noreply.github.com>
2021-12-26 23:33:36 +0100

    Still testing

--- Script2.js
+++ Script2.js
@@ -2,48 +2,179 @@
 
 $( document ).ready(function() {
 
-    for(let i = 0; i<= 10; i++){
-       popUp1 = new PopUp('Lailololeilo lala',  { my: "right+" + i*10 + " top-" + i*10 + "%"});
-       popUp1.showPopUp();
+    //Creating grid for background
 
+    for(let i = 0; i< 15; i++){
+        $( "#square_ref" ).clone().appendTo( "#grid_container" ).show();
     }
+         (function() {
+			[].slice.call(document.querySelectorAll('.grid--effect-vega > .grid__item')).forEach(function(stackEl) {
+				new VegaFx(stackEl);
+			});
+
+		})();
     
+//Test
+    popUpStart = new PopUp1('Pop up 1',  { my: "center", at: "left"}, "Ignore", function(){$( this ).dialog( "close" ); });
+    popUpStart.showPopUp();
+    
+    popUpStart = new PopUp2('Pop up 2',  { my: "center", at: "center"}, "Ignore", function(){$( this ).dialog( "close" ); }, "Ignore", function(){$( this ).dialog( "close" ); });
+    popUpStart.showPopUp();
+
+    popUpStart = new PopUp3('Pop up 3',  { my: "center", at: "right"}, "Ignore", function(){$( this ).dialog( "close" ); });
+    popUpStart.showPopUp();
+
+        
+    //Counts how many times mouse is over a square    
+    var n = 0;
+    
+    $( "div.stack" )
+      .mouseenter(function() {
+        n += 1;
+   
+      //Popups every n times
+
+      //First popup
+      if (n == 9)
+      {
+          popUpStart = new PopUp1('This is going well. So far.',  { my: "center", at: "center"}, "Ok", function(){$( this ).dialog( "close" ); });
+          popUpStart.showPopUp();
+      }
+
+      //First popup
+      if (n == 19)
+      {
+          popUpStart = new PopUp1('But is it actually going well?',  { my: "center", at: "center"}, "Sure", function(){$( this ).dialog( "close" ); });
+          popUpStart.showPopUp();
+      }
+
+      //popup in a loop times
+      if ((n % 10 == 0)&&(n/20 > 2)&&(n/20 < 4))
+      {
+          popUpStart = new PopUp1('You should be taking care of more important stuff, but still here you are',  { my: "center", at: "center"}, "Ignore", function(){$( this ).dialog( "close" ); });
+          popUpStart.showPopUp();
+          
+
+          //  popUpStart = new PopUp2('Something is not right',  { my: "center", at: "center"});
+          //  popUpStart.showPopUp();
+   
+      }
+
+
+      console.log("n: " + n );
+     });
+
+
+
+ 
+
+
+
+    //let n = 0;
+
+    //for(let i = 0; i<= 5; i++){
+     //  popUpStart = new PopUp1('Lailololeilo lala',  { my: "center", at: "bottom-" + n+ "%"});
+    //   popUpStart.showPopUp();
+    //   n = n+10;
+      // popUpStart2 = new PopUp2('Lailololeilo lala NO X',  { my: "right+" + n*10 + " top-" + n*10 + "%"});
+      // popUpStart2.showPopUp();
+   // }
+    
+
+
+
 });
 
-class PopUp {
-  constructor(message, position) {
+//PopUp1: with closing button (X) and 1 button
+class PopUp1 {
+  constructor(message, position, button1text, button1function) {
     this.message = message;
     this.position = position;
 
     this.showPopUp = function() {
         $('#popups').prepend('<div id="dialog" title="ERROR"><p>' + message + '</p></div>');    
          $("#dialog").dialog({
-         // modal: true,
-          hide: { effect: "explode", duration: 1000 },
-          show: { effect: "blind", duration: 800 },
+          modal: true,
+          hide: { effect: "slide", duration: 500 },
+          show: { effect: "bounce", duration: 400 },
           draggable: true,
           position: position,
           buttons: 
             [
                 {
-                  text: "OK",
-                  click: function() {
-                    $( this ).dialog( "close" );
-                  }
-                },
-                {
-                  text: "NOT OK",
-                  icon: "ui-icon-heart",
-                  click: function() {
-                   // $( this ).dialog( "close" );
-                   // newPopupA("This is going wrong " + i);
-                  }
+                  text: button1text,
+                  click: button1function                
                 }
             ]
 
         }).prev(".ui-dialog-titlebar").css("background","red"); 
       };
+
+
+  }
+}
+
+//PopUp2: with closing button (X) and 2 buttons
+class PopUp2 {
+  constructor(message, position, button1text, button1function, button2text, button2function) {
+    this.message = message;
+    this.position = position;
+
+    this.showPopUp = function() {
+        $('#popups').prepend('<div id="dialog" title="ERROR"><p>' + message + '</p></div>');    
+         $("#dialog").dialog({
+          modal: true,
+          hide: { effect: "slide", duration: 500 },
+          show: { effect: "bounce", duration: 400 },
+          draggable: true,
+          position: position,
+          buttons: 
+            [
+                {
+                  text: button1text,
+                  click: button1function
+                },
+                {
+                  text: button2text,
+                 // icon: "ui-icon-heart",
+                  click: button2function
+                }
+            ]
+
+        }).prev(".ui-dialog-titlebar").css("background","red"); 
+      };
+
+
   }
 }
 
 
+
+//PopUp3: No closing button (X), 1 button
+class PopUp3 {
+  constructor(message, position, button1text, button1function) {
+    this.message = message;
+    this.position = position;
+
+    this.showPopUp = function() {
+        $('#popups').prepend('<div id="dialog" title="ERROR"><p>' + message + '</p></div>');    
+         $("#dialog").dialog({
+          modal: true,
+          hide: { effect: "slide", duration: 500 },
+          show: { effect: "bounce", duration: 400 },
+          draggable: true,
+          position: position,
+          buttons: 
+            [
+                {
+                  text: button1text,
+                  click: button1function
+                }
+            ]
+
+        }).prev(".ui-dialog-titlebar").css("background","red").children(".ui-dialog-titlebar-close").hide(); 
+      };
+
+
+  }
+}
