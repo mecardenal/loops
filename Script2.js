6645972cf5f14a47cbdc14fa146e8b10ad0dd578
mecardenal <44734512+mecardenal@users.noreply.github.com>
2022-01-09 14:41:29 +0100

    puzzle shuffles

--- Script2.js
+++ Script2.js
@@ -2,17 +2,14 @@
 
 $( document ).ready(function() {
 
-    //Creating grid for background
+    GeneratePuzzlePieces();
+    chufle();
+        // (function() {
+		//	[].slice.call(document.querySelectorAll('.grid--effect-vega > .grid__item')).forEach(function(stackEl) {
+		//		new VegaFx(stackEl);
+		//	});
 
-    for(let i = 0; i< 56; i++){
-        $( "#square_ref" ).clone().appendTo( "#grid_container" ).show();
-    }
-         (function() {
-			[].slice.call(document.querySelectorAll('.grid--effect-vega > .grid__item')).forEach(function(stackEl) {
-				new VegaFx(stackEl);
-			});
-
-		})();
+		//})();
     
     //Test
     //popUpStart = new PopUp1('Pop up 1',  { my: "center", at: "left"}, "Ignore", function(){$( this ).dialog( "close" ); });
@@ -30,7 +27,9 @@ $( document ).ready(function() {
     
     $( "div.stack" )
       .mouseenter(function() {
-        n += 1;
+      
+      
+      n += 1;
    
       //Popups every n times
 
@@ -52,18 +51,10 @@ $( document ).ready(function() {
       //if ((n % 5 == 0)&&(n/10 > 2)&&(n/10 < 4))
       if (n == 25)
       {
-          popUpStart = new PopUp3('You should be taking care of more important stuff, but still here you are',  { my: "center", at: "center"}, "Ignore", function(){$( this ).dialog( "close" ); });
+          popUpStart = new PopUp3("Oh you are trying to ignore me, that's cute <3",  { my: "center", at: "center"}, "Ignore", function(){$( this ).dialog( "close" ); });
           popUpStart.showPopUp();
    
       }
-
-      if(n == 40){
-          //popUpStart = new PopUp3('You should be taking care of more important stuff, but still here you are',  { my: "center", at: "center"}, "Ignore", function(){$( this ).dialog( "close" ); });
-      }
-      //Loop intensifies
-     // if ((n % 10 == 0)&&(n/20 > 2)&&(n/20 < 4))
-
-     // }
 
 
      // console.log("n: " + n );
@@ -91,55 +82,38 @@ $( document ).ready(function() {
 });
 
 
-var numLoops = 0;
-//PopUp1: with closing button (X) and 1 button
-//Closing button generates new popups
-class PopUp3 {
-  constructor(message, position) 
-  {
+
+//PopUp1: No closing button (X), 1 button
+class PopUp1 {
+  constructor(message, position, button1text, button1function) {
     this.message = message;
     this.position = position;
 
     this.showPopUp = function() {
-        $('#popups').prepend('<div id="dialog" title="ERROR"><p>' + message + '</p></div>');    
-        $("#dialog").dialog({
-          //modal: true,
+        $('#popups').prepend('<div id="dialog" title="System Message"><p>' + message + '</p></div>');    
+         $("#dialog").dialog({
+          modal: true,
           hide: { effect: "slide", duration: 500 },
           show: { effect: "bounce", duration: 400 },
           draggable: true,
-          position: position
+          position: position,
+          buttons: 
+            [
+                {
+                  text: button1text,
+                  click: button1function
+                }
+            ]
 
-        }).prev(".ui-dialog-titlebar").css("background","red").children(".ui-dialog-titlebar-close").on("click",function(event, ui)
-          {
-
-
-            if(numLoops < 5)
-            {
-              for(var i = 0; ((i <= numLoops) && (i < 5)) ; i++)
-              {
-                  setTimeout(replicatePopUp,1000,message); 
-              }
-              numLoops++;
-            }
-            else
-            {
-              for(var i = 0; (i <= 40) ; i++)
-              {
-//                  setTimeout(replicatePopUp,1000,message); 
-                                    replicatePopUp(message);
-              }
-              setTimeout(blueScreen,4000); 
-                //console.log("MAUYOR QUE 5");
-               // replicatePopUp(message,"30");  
-            }
-              
-          
-          }); 
+        }).prev(".ui-dialog-titlebar").css("background","#ff3e30").css("color","white"); 
       };
 
 
   }
 }
+
+
+
 
 //PopUp2: with closing button (X) and 2 buttons
 class PopUp2 {
@@ -163,7 +137,7 @@ class PopUp2 {
                 }
             ]
 
-        }).prev(".ui-dialog-titlebar").css("background","red").next().next().children().mouseover(function()
+        }).prev(".ui-dialog-titlebar").css("background","#ff3e30").css("color","white").next().next().children().mouseover(function()
             {
                  $( this ).parent().css("float","left");
             }).mouseout(function()
@@ -177,31 +151,52 @@ class PopUp2 {
   }
 }
 
-               
+              
 
-//PopUp1: No closing button (X), 1 button
-class PopUp1 {
-  constructor(message, position, button1text, button1function) {
+var numLoops = 0;
+//PopUp1: with closing button (X) and 1 button
+//Closing button generates new popups
+class PopUp3 {
+  constructor(message, position) 
+  {
     this.message = message;
     this.position = position;
 
     this.showPopUp = function() {
-        $('#popups').prepend('<div id="dialog" title="ERROR"><p>' + message + '</p></div>');    
-         $("#dialog").dialog({
+        $('#popups').prepend('<div id="dialog" title="FATAL ERROR"><div class="popup_content"><img src="img/Flat_cross_icon.svg" width="30px" class="error_icon"><div class="error_message">' + message + '</div></div></div>');    
+        $("#dialog").dialog({
           modal: true,
           hide: { effect: "slide", duration: 500 },
           show: { effect: "bounce", duration: 400 },
-          draggable: true,
-          position: position,
-          buttons: 
-            [
-                {
-                  text: button1text,
-                  click: button1function
-                }
-            ]
+          //draggable: true,
+          position: position
+          
+        }).prev(".ui-dialog-titlebar").css("background","#ff3e30").css("color","#ffff30").children(".ui-dialog-titlebar-close").on("click",function(event, ui)
+          {
 
-        }).prev(".ui-dialog-titlebar").css("background","red"); 
+
+            if(numLoops < 5)
+            {
+              for(var i = 0; ((i <= numLoops) && (i < 5)) ; i++)
+              {
+                  setTimeout(replicatePopUp,1000,message); 
+              }
+              numLoops++;
+            }
+            else
+            {
+              for(var i = 0; (i <= 40) ; i++)
+              {
+//                  setTimeout(replicatePopUp,1000,message); 
+                    replicatePopUp(message);
+              }
+              setTimeout(blueScreen,4000); 
+                //console.log("MAUYOR QUE 5");
+               // replicatePopUp(message,"30");  
+            }
+              
+          
+          }); 
       };
 
 
@@ -211,7 +206,7 @@ class PopUp1 {
 
 function blueScreen() 
 {
-    $(".ui-dialog").hide();
+    $(".ui-dialog").remove();
     $("#blue_screen").show();
 }
 
@@ -225,6 +220,52 @@ function replicatePopUp(message)
         repeatPU.showPopUp();
 
               
+}
+
+//Restart game in safe mode on clicking the button
+$("#safemode_button").on("click",function(){
+    $("#blue_screen").remove();
+    $(".safemode").show();
+});
+
+function GeneratePuzzlePieces()
+{
+    //Creating grid for puzzle
+
+    var numH = 0;
+    var idPiece = 1;
+    var leftPX = 0;
+    var topPX = 0;
+
+    for(let i = 0; i< 28; i++){
+        
+        //Creates pieces of puzzle 
+        $( "#square_ref" ).clone().appendTo( "#grid_container" ).show().find(".puzzle_piece").css("background-image","url(img/sketch_brainloop.png)").css("background-position","left "+ leftPX +"px top "+ topPX +"px").parent().parent().parent().attr('id', idPiece);
+       
+        //Starts horizontal iteration
+        if(numH < 6) 
+        {
+            leftPX = leftPX - 110;
+            numH++;
+        }
+        else //Line break
+        {
+            numH = 0;
+            leftPX = 0;
+            topPX = topPX -110;
+        }
+       
+        idPiece++;
+    }
+}
+
+//Shuffle the pieces of the grid
+function chufle() {
+    var parent = $("#grid_container");
+    var divs = parent.children();
+    while (divs.length) {
+        parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+    }
 }
 
 //Disable right click
