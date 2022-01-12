11063ab4acd1d85ecf6dd517a89b5ddbe077ddf1
mecardenal <44734512+mecardenal@users.noreply.github.com>
2022-01-12 20:38:54 +0100

    puzzle va

--- Script2.js
+++ Script2.js
@@ -3,7 +3,8 @@
 $( document ).ready(function() {
 
     GeneratePuzzlePieces();
-    chufle();
+    //chufle();
+    assignPosPiece();
     
         // (function() {
 		//	[].slice.call(document.querySelectorAll('.grid--effect-vega > .grid__item')).forEach(function(stackEl) {
@@ -38,14 +39,14 @@ $( document ).ready(function() {
       if (n == 9)
       {
           popUpStart = new PopUp1('This is going well. So far.',  { my: "center", at: "center"}, "Ok", function(){$( this ).dialog( "close" ); });
-         // popUpStart.showPopUp();
+        //  popUpStart.showPopUp();
       }
 
       //First popup
       if (n == 19)
       {
           popUpStart = new PopUp2('But is it actually going well?',  { my: "center", at: "center"}, "Sure");
-        //  popUpStart.showPopUp();
+      //    popUpStart.showPopUp();
       }
 
       //popup x times
@@ -85,34 +86,18 @@ $( document ).ready(function() {
             if(pBsel == 1)
             {
                 //switch pieces
-                //console.log("switch");
                 switchPieces(idpA,idpB);
                 //deselect pieces
                 pAsel = 0;
                 pBsel = 0; 
                 idpA = 0;
                 ipdB = 0;
-                
+                $("a[issel|='selA']").removeAttr("issel");
+                $("a[issel|='selB']").removeAttr("issel");
+               
             }
-
         }
-       
-       //console.log("selA " + idpA + " / selB " + idpB);
-
-
-
-        //Check succes of puzzle
-       // var suc = checkSuccess();
-       // if(suc == true)
-       // {
-            //the end stage
-       // }
-
-
     });  
-
-
-
 });
 
 
@@ -275,7 +260,7 @@ function GeneratePuzzlePieces()
     for(let i = 0; i< 28; i++){
         
         //Creates pieces of puzzle 
-        $( "#square_ref" ).clone().appendTo( "#grid_container" ).show().find(".puzzle_piece").css("background-image","url(img/sketch_brainloop.png)").css("background-position","left "+ leftPX +"px top "+ topPX +"px").parent().parent().parent().attr('idPiece', idPiece).attr('id', '');
+        $( "#square_ref" ).clone().appendTo( "#grid_container" ).show().find(".puzzle_piece").css("background-image","url(img/sketch_brainloop_guideColor.png)").css("background-position","left "+ leftPX +"px top "+ topPX +"px").parent().parent().parent().attr('idPiece', idPiece).attr('id', '');
 
         //Starts horizontal iteration
         if(numH < 6) 
@@ -296,6 +281,21 @@ function GeneratePuzzlePieces()
 
 var orderPieces = [];
 
+
+/*
+2, 5, 7, 8, 9
+
+clicked = 5; clickedPosInArr = 1; 
+flagFirstClick = true;
+2clicked = 8; 2clickedPosInArr = 3;
+array[clickedPosInArr] = 2clicked;
+array[2clickedPosInArr] = clicked;
+
+2, 8, 7, 5, 9
+*/
+
+
+
 //Shuffle the pieces of the grid
 function chufle() {
     var parent = $("#grid_container");
@@ -304,47 +304,73 @@ function chufle() {
     while (divs.length) {
        parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
     }
-    
+
     //I save the position on the grid and the order of the pieces
-    var j = 1;
+    var j = 0;
     $("#grid_container").children(".grid__item").each(function(){
-        orderPieces[(j - 1)] = $(this).attr('idpiece');
+
         $(this).attr('posPiece', j);
+        orderPieces[j] = $(this).attr('idpiece');
         j++;
     });
+
 }
 
 
 function assignPosPiece(){
-    var j = 1;
-     $("#grid_container").children(".grid__item").each(function(){
+    var j = 0;
+    $("#grid_container").children(".grid__item").each(function(){
+
         $(this).attr('posPiece', j);
+        orderPieces[j] = $(this).attr('idpiece');
         j++;
-     });
+    });
 }
 
 //Switch 2 selected pieces
 function switchPieces(idpA,idpB) {
 
+  
     var posA = $("a[idpiece|='" + idpA + "']").attr("pospiece");
     var posB = $("a[idpiece|='" + idpB + "']").attr("pospiece");
 
+    orderPieces[posA] = idpB;
+    orderPieces[posB] = idpA;
+ 
+    //I create a temp grid container
+    $('.content').append('<div id="grid_container" class="grid grid--effect-vega"></div>');  
 
-   // $("a[idpiece|='" + idpA + "']").insertBefore( "a[pospiece|='" + posB + "']" );
-   // $("a[idpiece|='" + idpB + "']").insertAfter( "a[pospiece|='" + posA + "']" );
-    //var mylist = $('#grid_container');
-  //  [orderPieces[posA], orderPieces[posB]] = [orderPieces[posB], orderPieces[posA]];
-    console.log("posA " + posA + " posB " + posB);
-
-   //// var parent = $("#grid_container");
-  //  var divs = parent.children();
-
-  //  while (divs.length) {
-    //   parent.append(divs.splice(divs.length, 1)[0]);
-   // }
+    for(i = 0; i < orderPieces.length; i++){
+    //    console.log("idpiece " + (i+1) + " - " + orderPieces[i]);
+        $("#grid_container").append($("a[idpiece|='" + orderPieces[i] + "']"));
+        $("a[idpiece|='" + orderPieces[i] + "']").attr('posPiece', i);
+    }
+    checkSuccess();
 }
 
+function checkSuccess(){
+console.log("CHECKING");
+    var j = 1;
+    var id;
+    var success = true;
 
+    $("#grid_container").children(".grid__item").each(function(){
+        
+        var id = $(this).attr("idpiece");
+
+        if(id !=  j ){
+            success = false;
+        }
+        j++;
+
+       
+    });
+
+    if(success == true){
+    alert( "TIMON LEDEEEER");
+    }
+
+}
 
 
 //Disable right click
